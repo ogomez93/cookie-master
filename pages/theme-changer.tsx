87d1a39f1938595acaa392d5a3c3fa1a3adc0c7c
Oswaldo Gomez <oswaldogomezsl@gmail.com>
2023-05-28 21:41:21 -0300

    ThemeChanger sets value to cookies and gets initial value from cookies from server side props

--- pages/theme-changer.tsx
+++ pages/theme-changer.tsx
@@ -1,14 +1,23 @@
-import { NextPage } from 'next'
 import { ChangeEvent, useState } from 'react'
+import { GetServerSideProps, NextPage } from 'next'
+import Cookies from 'js-cookie'
 
 import { Card, CardContent, FormControl, FormControlLabel, FormLabel, Radio, RadioGroup } from '@mui/material'
 
 import { Layout } from '../components/layouts'
 
-const ThemeChanger: NextPage = () => {
-  const [currentTheme, setCurrentTheme] = useState('light')
+interface Props {
+  theme: 'light' | 'dark' | 'custom'
+}
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => setCurrentTheme(e.target.value)
+const ThemeChanger: NextPage<Props> = ({ theme }) => {
+  const [currentTheme, setCurrentTheme] = useState(theme)
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    const selectedTheme = e.target.value as Props['theme']
+    setCurrentTheme(selectedTheme)
+    Cookies.set('COOKIE_MASTER-theme', selectedTheme)
+  }
 
   return (
     <Layout>
@@ -26,6 +35,18 @@ const ThemeChanger: NextPage = () => {
       </Card>
     </Layout>
   )
+}
+
+
+
+export const getServerSideProps: GetServerSideProps = async ({ req }) => {
+  const { 'COOKIE_MASTER-theme': theme = 'light' } = req.cookies
+
+  return {
+    props: {
+      theme
+    }
+  }
 }
 
 export default ThemeChanger
